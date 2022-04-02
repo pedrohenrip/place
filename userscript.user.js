191107777380f8eaac965a620a77e37a3c153c24
pedrohenrip <35971721+pedrohenrip@users.noreply.github.com>
2022-04-02 17:20:43 -0300

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         SuperStonk Logo template
+// @name         rbrasil
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the canvas!
-// @author       oralekin
+// @author       naosei
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
@@ -14,7 +14,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             const time = Math.floor(Date.now() / 10000);
-            i.src = "https://raw.githubusercontent.com/rplacesuperstonk/rplace-image/main/superstonk_overlay.png?tstamp=" + time;
+            i.src = "https://raw.githubusercontent.com/pedrohenrip/place/main/Untitled.png?tstamp=" + time;
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
